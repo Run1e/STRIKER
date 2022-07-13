9824a6a24e1ef5a6b37ed963d6fe7c9f6efed378
runie <example@example.com>
2022-07-13 22:59:28 +0200

    test: Add dummy database and amqp bind to example bot config

--- bot/example_config.py
+++ bot/example_config.py
@@ -3,10 +3,10 @@ DROP_TABLES = False
 DUMP_EVENTS = False
 
 BOT_TOKEN = None
-DB_BIND = None
+DB_BIND = 'postgresql+asyncpg://user:pass@host:5432/database'
 TEST_GUILDS = []
 
-RABBITMQ_HOST = None
+RABBITMQ_HOST = 'amqp://user:pass@host:5672'
 QUEUE_PREFIX = ''
 
 VIDEO_DIR = ''
