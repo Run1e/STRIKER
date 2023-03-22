acefb43171d6b1e644ff9970310498758c784792
runie <runar-borge@hotmail.com>
2023-03-23 00:06:57 +0100

    build: Docker stuff

---
+++ build_images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t striker-bot -f bot/Dockerfile .
+docker build -t striker-demoparse -f microservices/demoparse/Dockerfile .
+docker build -t striker-login-provisioner -f microservices/login-provisioner/Dockerfile .
+docker build -t striker-matchinfo -f microservices/matchinfo/Dockerfile .
+docker build -t striker-uploader -f microservices/uploader/Dockerfile .
