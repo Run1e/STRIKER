8106930fe6f10c42ff82711c71fe220c4cb89796
runie <runar-borge@hotmail.com>
2023-05-14 04:04:21 +0200

    feat: Disable domain tests and update bot example config

--- bot/example_config.py
+++ bot/example_config.py
@@ -2,18 +2,39 @@ DEBUG = False
 DROP_TABLES = False
 DUMP_EVENTS = False
 
-BOT_TOKEN = None
-DB_BIND = "postgresql+asyncpg://user:pass@host:5432/database"
-TEST_GUILDS = []
+DB_BIND = 'postgresql+asyncpg://user:pass@host:5432/database'
+BOT_TOKEN = ""
+FACEIT_API_KEY = ""
+STEAM_REFRESH_TOKEN = ""
+STRIKER_GUILD_ID = int()
+TEST_GUILDS = [STRIKER_GUILD_ID]
 
-RABBITMQ_HOST = "amqp://user:pass@host:5672"
-QUEUE_PREFIX = ""
+RABBITMQ_HOST = 'amqp://user:pass@host:5672'
+VIDEO_UPLOAD_URL = ""
 
-VIDEO_DIR = ""
+JOB_LIMIT = 1
+
+NODE_TOKENS = {"token"}
 
 CT_COIN = ""
 T_COIN = ""
 SPINNER = ""
 
-GITHUB_URL = None
-DISCORD_INVITE_URL = "https://discord.gg/akAyfP9z53"
+SHARECODE_IMG_URL = "https://i.imgur.com/e68ERoQ.png"
+DONATE_URL = "https://ko-fi.com/strikerbot"
+TRADELINK_URL = "https://steamcommunity.com/tradeoffer/new/?partner=83930225&token=Nv7DHv4t"
+DISCORD_INVITE_URL = "https://discord.gg/G7cMssWnR2"
+GITHUB_URL = "https://github.com/Run1e/STRIKER"
+PATREON_URL = "https://www.patreon.com/strikerbot"
+
+PATREON_TIERS = {
+    1: (int(),),
+    2: (int(),),
+    3: (int(),),
+}
+
+PATREON_TIER_NAMES = {
+    1: "",
+    2: "",
+    3: "",
+}
