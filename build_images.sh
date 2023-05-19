fdd1be73d02c80a214bf9f7ec1752ad92f9b1641
runie <runar-borge@hotmail.com>
2023-05-19 08:13:53 +0200

    build: Update dockerfiles and requirements

--- build_images.sh
+++ build_images.sh
@@ -2,5 +2,5 @@
 
 docker build -t striker-bot -f bot/Dockerfile .
 docker build -t striker-demoparse -f microservices/demoparse/Dockerfile .
-docker build -t striker-login-provisioner -f microservices/login-provisioner/Dockerfile .
+docker build -t striker-gateway -f microservices/gateway/Dockerfile .
 docker build -t striker-uploader -f microservices/uploader/Dockerfile .
